5a12059e68e75ef5d087a056bcd29bdd1db58d20
Dave <daaaaaavis@gmail.com>
2018-08-05 14:24:05 +0300

    First commit

---
+++ hypersonic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]);
+        int height = int.Parse(inputs[1]);
+        int myId = int.Parse(inputs[2]);
+
+        // game loop
+        while (true)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                string row = Console.ReadLine();
+            }
+            int entities = int.Parse(Console.ReadLine());
+            for (int i = 0; i < entities; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int entityType = int.Parse(inputs[0]);
+                int owner = int.Parse(inputs[1]);
+                int x = int.Parse(inputs[2]);
+                int y = int.Parse(inputs[3]);
+                int param1 = int.Parse(inputs[4]);
+                int param2 = int.Parse(inputs[5]);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("BOMB 6 5");
+        }
+    }
+}
