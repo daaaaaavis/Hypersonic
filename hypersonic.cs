c1a147ca2247344a098e3b281ea04bf2d80d7a41
Dave <daaaaaavis@gmail.com>
2018-08-06 22:54:28 +0300

    No Thinkpada

---
+++ hypersonic.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        
+        Grid grid = new Grid();         // spēles stāvoklis šobrīd
+        Grid futureGrid = new Grid();   // spēles stāvoklis, kas ir 'garantēti' nākotnē
+        Coordinates playerCoord = new Coordinates();
+               
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]);
+        int height = int.Parse(inputs[1]);
+        int myId = int.Parse(inputs[2]);
+
+        // game loop
+        while (true)
+        {
+            for (int i = 0; i < height; i++) // height lines: a string row representing each row of the grid. Each character can be: "." an empty cell, "0" a box.
+            {
+                string row = Console.ReadLine();
+                grid.refreshGrid(row, i);
+                // futureGrid.refreshGrid(row, i);
+            }
+            // grid.printGrid();
+            int entities = int.Parse(Console.ReadLine());
+            for (int i = 0; i < entities; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int entityType = int.Parse(inputs[0]);
+                int owner = int.Parse(inputs[1]);
+                int x = int.Parse(inputs[2]);
+                int y = int.Parse(inputs[3]);
+                int param1 = int.Parse(inputs[4]);
+                int param2 = int.Parse(inputs[5]);
+
+                if (entityType == 0 && owner == 0)
+                {
+                    playerCoord.x = x;
+                    playerCoord.y = y;
+                    // Mans spēlētājs
+                }
+
+                if (entityType == 1)
+                {
+                    // izreķināt futureGrid laukus
+                    // param2 - explosion range for bombs
+                    // ja ir bumba, vienkārši ar range sanuļļot apkārt
+
+
+                }
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+            grid.fillAdjacentBoxesArray(2);
+            
+            // grid.selectClosestBox();
+            // Box box = grid.selectClosestBox();
+            Coordinates coords = grid.getBestCoordinates(playerCoord);
+            string nextCommand = "BOMB " + coords.y + " " + coords.x;
+            
+            
+            grid.printAdjacent();
+            Console.WriteLine(nextCommand);
+        }
+    }
+}
+
+class Grid
+{
+    private char[,] gameArray = new char[11,13];
+    private int[,] adjacentBoxesArray = new int[11, 13];
+    private Coordinates nextBox = new Coordinates();
+    
+    public void refreshGrid(string row, int number)
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            gameArray[number, i] = row[i];
+        }
+    }
+    
+    public Coordinates selectClosestBox()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+             if (!DoubleLoop(i)) break;
+        }
+        return nextBox;
+    }
+    public bool DoubleLoop(int i)
+    {
+        for (int j = 0; j < 13; j++)
+         {
+             // Console.Error.Write(gameArray[i,j]);
+             if (!gameArray[i,j].Equals('.'))
+             {
+                 nextBox.y = i;
+                 nextBox.x = j;
+                 return false;
+             }
+         }
+         return true;
+    }
+
+    public int distance(Coordinates player, Coordinates point) // finds what is the distance between two points
+    {
+        var dx = player.x - point.x;
+        var dy = player.y - point.y;
+        return Math.Abs(dx) + Math.Abs(dy);
+    }
+    
+    public void printGrid()
+    {
+        string row = String.Empty;
+        
+        for (int i = 0; i < 11; i++)
+        {
+         for (int j = 0; j < 13; j++)
+         {
+             row = row + gameArray[i,j] + " ";
+         }
+         Console.Error.WriteLine(row);
+         row = String.Empty;
+        }
+    }
+
+    public void fillAdjacentBoxesArray(int bombRange)
+    {
+        int indexUp; // par cik var iet uz attiecīgo pusi
+        int indexDown;
+        int indexLeft;
+        int indexRight;
+        int adjacentCount = 0;
+
+        for (int i = 0; i < 11; i++) // rindas
+        {
+         for (int j = 0; j < 13; j++) // kolonnas
+         {
+             if (!gameArray[i,j].Equals('.'))
+             {
+                 adjacentBoxesArray[i,j] = 0;
+                 continue;
+             }
+            // REWRITE THIS S**T
+            // if ( i < bombRange )
+            // {
+            //     indexUp = i;
+            // }
+            // else 
+            // {
+            //     indexUp = bombRange;
+            // }
+
+            // if ( j < bombRange )
+            // {
+            //     indexLeft = j;
+            // }
+            // else
+            // {
+            //     indexLeft = bombRange;
+            // }
+
+            // if ( i > (10-bombRange) )
+            // {
+            //     indexDown = 10 - i;
+            // } 
+            // else
+            // {
+            //     indexDown = bombRange;
+            // }
+
+            // if ( j > (12-bombRange) )
+            // {
+            //     indexRight = 12 - j;
+            // }
+            // else
+            // {
+            //     indexRight = bombRange;
+            // }
+
+
+            if ( i == 0 ) {
+                indexUp = 0;
+            } else if ( i == 1){
+                indexUp = 1;
+            } else {
+                indexUp = 2;
+            }
+
+            if ( j == 0 ) {
+                indexLeft = 0;
+            } else if ( j == 1){
+                indexLeft = 1;
+            } else { 
+                indexLeft = 2;
+            }
+
+            if ( i == 10 ) {
+                indexDown = 0;
+            } else if ( i == 9){
+                indexDown = 1;
+            } else {
+                indexDown = 2;
+            }
+
+            if ( j == 12 ) {
+                indexRight = 0;
+            } else if ( j == 11){
+                indexRight = 1;
+            } else {
+                indexRight = 2;
+            }     
+            // Console.Error.WriteLine(i + " " + j);
+            // CHECK HOW MANY BOXES ARE ADJACENT
+            for (int k = 1; k < indexUp+1; k++)
+            {
+                if(gameArray[i-k,j].Equals('0')) adjacentCount++;
+            }
+            // Console.Error.WriteLine(indexDown.ToString() + " " + i);
+            for (int k = 1; k < indexDown+1; k++)
+            {
+                if(gameArray[i+k,j].Equals('0')) adjacentCount++;
+            }
+
+            for (int k = 1; k < indexLeft+1; k++)
+            {
+                if(gameArray[i,j-k].Equals('0')) adjacentCount++;
+            }
+
+            for (int k = 1; k < indexRight+1; k++)
+            {
+                if(gameArray[i,j+k].Equals('0')) adjacentCount++;
+            }
+            adjacentBoxesArray[i,j] = adjacentCount;
+            adjacentCount = 0;
+         }
+        }
+    }
+    // END FILL ADJACENT BOXES
+
+    public void printAdjacent()
+    {
+        string row = String.Empty;
+        
+        for (int i = 0; i < 11; i++)
+        {
+         for (int j = 0; j < 13; j++)
+         {
+             row = row + adjacentBoxesArray[i,j] + " ";
+         }
+         Console.Error.WriteLine(row);
+         row = String.Empty;
+        }
+    }
+
+    public Coordinates getBestCoordinates(Coordinates player)
+    {
+        int maxCount = 0;
+        List<Coordinates> list = new List<Coordinates>();
+        for (int i = 0; i < 11; i++) // rindas
+        {
+         for (int j = 0; j < 13; j++) // kolonnas
+         {
+             if (adjacentBoxesArray[i,j] == maxCount) 
+             {
+                 Coordinates temp = new Coordinates(i,j);
+                 list.Add(temp);
+             }
+             if (adjacentBoxesArray[i,j] > maxCount)
+             {
+                 maxCount = adjacentBoxesArray[i,j];
+                 list.Clear();
+                 Coordinates temp = new Coordinates(i,j);
+                 temp.surroundingBoxes = maxCount;
+                 list.Add(temp);
+             } 
+         }
+        }
+        // IR LISTS AR VISĀM "LABĀKAJĀM" KOORD.
+        // jāskatās, kurš punkts vistuvāk
+        Coordinates closest = new Coordinates();
+        int closestDistance = 0;
+        int tempDistance = 0;
+
+        closest.x = list[0].x;
+        closest.y = list[0].y;
+        closestDistance = distance(player, closest);
+
+        Console.Error.WriteLine("Error: " + closest.x + " " + closest.y + " " + closestDistance);
+        Console.Error.WriteLine("Player: " + player.x + " " + player.y);
+        Console.Error.WriteLine("Closest: " + closest.x + " " + closest.y);
+
+        for (int i = 0; i < list.Count(); i++)
+        {
+            tempDistance = distance(list[i], player);
+            if (tempDistance < closestDistance)
+            {
+                closest.x = list[i].x;
+                closest.y = list[i].y;
+                closestDistance = tempDistance;
+                Console.Error.WriteLine("Error2: " + closest.x + " " + closest.y + " " + closestDistance);
+            }
+        }
+
+        Console.Error.WriteLine("ErrorFinal: " + closest.x + " " + closest.y + " " + closestDistance);
+        return closest;
+    }
+}
+
+
+class Coordinates
+{
+    public int x;
+    public int y;
+    public int surroundingBoxes;
+
+    public Coordinates()
+    {  
+    }
+
+    public Coordinates(int X, int Y)
+    {
+        x = X;
+        y = Y;
+    }
+}
+
+
+
---
+++ voronoi.js
@@ -0,0 +1,41 @@
+﻿// link - https://tech.io/playgrounds/243/voronoi-diagrams/lets-get-your-hands-dirty
+// Create the Voronoi areas
+
+function fillVoronoi (tiles, players) {
+    /*
+      Modify the `site` property of *each* tile to be a *reference* to one of the players
+      given as parameter. The tile must be in that player's Voronoi site.
+      ex:
+        tiles[0].site = players[0];
+        tiles[1].site = players[1];
+    */
+    var minDistance = 0;
+  
+    for(var t in tiles) {
+      var tile = tiles[t];
+      
+      minDistance = distance(tile, players[0]);
+      minDistancePlayer = 0;
+      
+      for (var i = 1; i < 4; i++)
+      {
+          var temp = distance(tile, players[i]);
+          if (temp < minDistance)
+          {
+              minDistance = temp;
+              minDistancePlayer = i;
+          }
+      }
+  
+      tile.site = player[minDistancePlayer];
+      // TODO: Select the nearest player, assigning it to tile.site
+    }
+  }
+  
+  function distance(pointA, pointB) {
+      var dx = pointA.x - pointB.x;
+      var dy = pointA.y - pointB.y;
+      return Math.abs(dx) + Math.abs(dy);
+    // TODO: Implement the manhattan distance function
+    // Both tiles and players have a `x` and a `y` property
+  }
